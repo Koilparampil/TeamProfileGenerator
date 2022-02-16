a676df16208e3a7889a1889d129bc07267d6ba67
KreshsDesktop <ckresh99@gmail.com>
2022-02-16 16:44:37 -0500

    made all the clases

---
+++ index.js
@@ -0,0 +1,10 @@
+const Employee  =require("./lib/Employee")
+const Engineer  =require("./lib/Engineer")
+const Intern  =require("./lib/Intern")
+const Manager =require("./lib/Manager")
+
+
+
+const Johnstopher= new Employee('johnstopher',293874,'mail@mail.com')
+
+console.log(Johnstopher)
